823e4b79be4538a5f7458226545c24240ca1021a
jdye <jeremy.dye@usg.edu>
2023-04-25 16:01:48 -0400

    installed ExpressionEngine

---
+++ system/ee/ExpressionEngine/Service/Validation/Rule/Writable.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * This source file is part of the open source project
+ * ExpressionEngine (https://expressionengine.com)
+ *
+ * @link      https://expressionengine.com/
+ * @copyright Copyright (c) 2003-2022, Packet Tide, LLC (https://www.packettide.com)
+ * @license   https://expressionengine.com/license Licensed under Apache License, Version 2.0
+ */
+
+namespace ExpressionEngine\Service\Validation\Rule;
+
+use ExpressionEngine\Library\Filesystem\Filesystem;
+use ExpressionEngine\Service\Validation\ValidationRule;
+
+/**
+ * Writable Validation Rule
+ */
+class Writable extends ValidationRule
+{
+    protected $all_values = array();
+
+    public function validate($key, $value)
+    {
+        return ee('Filesystem')->isWritable(parse_config_variables($value, $this->all_values));
+    }
+
+    public function getLanguageKey()
+    {
+        return 'invalid_path';
+    }
+
+    public function setAllValues(array $values)
+    {
+        $this->all_values = $values;
+    }
+}
+
+// EOF
